79e03870a57b6501904107f3aec0163801e7465f
Ulrichmike <mikeweda10@gmail.com>
2025-08-14 00:39:42 +0100

    Correction Erreur Typescript

--- src/components/CustomMap.tsx
+++ src/components/CustomMap.tsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import { Map, Marker } from "@vis.gl/react-google-maps";
+// import { useState } from "react";
+// import { Map, Marker } from "@vis.gl/react-google-maps";
 
-const CustomMap = () => {
-  // shows marker on London by default
-  const [markerLocation, setMarkerLocation] = useState({
-    lat: 51.509865,
-    lng: -0.118092,
-  });
+// const CustomMap = () => {
+//   // shows marker on London by default
+//   const [markerLocation, setMarkerLocation] = useState({
+//     lat: 51.509865,
+//     lng: -0.118092,
+//   });
 
-  return (
-    <div className="map-container h-[500px] border-black-500 rounded-2">
-      <Map
-        style={{ borderRadius: "20px" }}
-        defaultZoom={13}
-        defaultCenter={markerLocation}
-        gestureHandling={"greedy"}
-        disableDefaultUI
-      >
-        <Marker position={markerLocation} />
-      </Map>
-    </div>
-  );
-};
+//   return (
+//     <div className="map-container h-[500px] border-black-500 rounded-2">
+//       <Map
+//         style={{ borderRadius: "20px" }}
+//         defaultZoom={13}
+//         defaultCenter={markerLocation}
+//         gestureHandling={"greedy"}
+//         disableDefaultUI
+//       >
+//         <Marker position={markerLocation} />
+//       </Map>
+//     </div>
+//   );
+// };
 
-export default CustomMap;
+// export default CustomMap;
