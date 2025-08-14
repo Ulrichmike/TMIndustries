9fd447f813f4b585ff477846712a7d5c46489a0b
Ulrichmike <mikeweda10@gmail.com>
2025-08-14 10:44:25 +0100

    modifs forms

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -131,11 +131,21 @@ export default function Contact() {
             </label>
             <div className="type flex items-center justify-center gap-1 md:gap-10 md:ml-0 lg:ml-0">
               <label className="flex items-center gap-1">
-                <input type="radio" name="type" id="" />
+                <input
+                  type="radio"
+                  name="type"
+                  id=""
+                  className="flex font-medium text-gray-800"
+                />
                 Professionnel
               </label>
               <label className="flex items-center gap-1">
-                <input type="radio" name="type" id="" />
+                <input
+                  type="radio"
+                  name="type"
+                  id=""
+                  className="flex font-medium text-gray-800"
+                />
                 Particulier
               </label>
             </div>
@@ -143,47 +153,53 @@ export default function Contact() {
 
           <div className="personnal grid grid-cols-1 sm:grid-cols-2 sm:gap-10 lg:grid-cols-2 lg:gap-10">
             <div className="flex flex-col flex-1">
-              <label className="flex">Prenom :</label>
+              <label className="flex font-medium text-gray-800">Prenom :</label>
               <input
                 type="text"
                 name=""
                 id=""
                 placeholder="LastName"
-                className="border h-12 pl-10 rounded"
+                // className="border h-12 pl-10 rounded"
+                className="border border-gray-300 shadow-sm h-12 pl-10 rounded-md bg-gray-100 text-gray-900 placeholder-gray-500 focus:border-secondary"
                 required
               />
             </div>
             <div className="flex flex-col flex-1">
-              <label className="flex">Nom :</label>
+              <label className="flex font-medium text-gray-800">Nom :</label>
               <input
                 type="text"
                 name=""
                 id=""
                 placeholder="FirstName"
-                className="border h-12 pl-10 rounded"
+                // className="border h-12 pl-10 rounded"
+                className="border border-gray-300 shadow-sm h-12 pl-10 rounded-md bg-gray-100 text-gray-900 placeholder-gray-500 focus:border-secondary"
                 required
               />
             </div>
           </div>
           <div className="second_personnal grid grid-cols-1 sm:grid-cols-2 sm:gap-10 lg:grid-cols-2 lg:gap-10">
             <div className="flex flex-col w-full">
-              <label className="flex">Email : </label>
+              <label className="flex font-medium text-gray-800">Email : </label>
               <input
                 type="email"
                 name=""
                 id=""
                 placeholder="Email"
-                className="border h-12 pl-10 rounded"
+                // className="border h-12 pl-10 rounded"
+                className="border border-gray-300 shadow-sm h-12 pl-10 rounded-md bg-gray-100 text-gray-900 placeholder-gray-500 focus:border-secondary"
                 required
               />
             </div>
             <div className="flex flex-col w-full">
-              <label className="flex">Téléphone :</label>
+              <label className="flex font-medium text-gray-800">
+                Téléphone :
+              </label>
               <input
                 type="tel"
                 name=""
                 id=""
-                className="border h-12 pl-10 rounded"
+                // className="border h-12 pl-10 rounded"
+                className="border border-gray-300 shadow-sm h-12 pl-10 rounded-md bg-gray-100 text-gray-900 placeholder-gray-500 focus:border-secondary"
                 required
               />
             </div>
@@ -202,11 +218,12 @@ export default function Contact() {
             />
           </div>
           <div className="objet flex flex-col">
-            <label className="flex">Objet : </label>
+            <label className="flex font-medium text-gray-800">Objet : </label>
             <select
               name=""
               id=""
-              className="border h-12 px-10 rounded"
+              //   className="border h-12 px-10 rounded"
+              className="border border-gray-300 shadow-sm h-12 pl-10 rounded-md bg-gray-100 text-gray-900 placeholder-gray-500 focus:border-secondary"
               required
             >
               <option value="Demande de devis">Demande de devis</option>
@@ -217,16 +234,22 @@ export default function Contact() {
             </select>
           </div>
           <div className="message flex flex-col">
-            <label className="flex">Message</label>
+            <label className="flex font-medium text-gray-800">Message</label>
             <textarea
               name=""
               id=""
-              className="border h-20 rounded"
+              //   className="border h-20 rounded"
+              className="border border-gray-300 shadow-sm h-12 pl-10 rounded-md bg-gray-100 text-gray-900 placeholder-gray-500 focus:border-secondary"
               required
             ></textarea>
           </div>
           <div className="check py-2">
-            <input type="checkbox" name="check" id="" />
+            <input
+              type="checkbox"
+              name="check"
+              id=""
+              className="flex font-medium text-gray-800"
+            />
             J’ai lu et j’accepte les mentions légales liées à l’usage de mes
             données personnelles. En soumettant ce formulaire, j’accepte d’être
             recontacté(e) dans le cadre d’une demande d’information autour des
